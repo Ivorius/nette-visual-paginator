1592138a5100e9945ed3ec4fbf675d5de4f0d876
pvy <pvy@centrum.cz>
2013-03-04 21:51:51 +0100

    formatovani

--- VisualPaginator.php
+++ VisualPaginator.php
@@ -13,10 +13,8 @@
  * @package    Nette Extras
  * @version    $Id: VisualPaginator.php 4 2009-07-14 15:22:02Z david@grudl.com $
  */
-
 use Nette\Application\UI\Control;
 use Nette\Utils\Paginator;
-
 
 /**
  * Visual paginator control.
@@ -25,68 +23,58 @@ use Nette\Utils\Paginator;
  * @copyright  Copyright (c) 2009 David Grudl
  * @package    Nette Extras
  */
-class VisualPaginator extends Control
-{
-	/** @var Paginator */
-	private $paginator;
+class VisualPaginator extends Control {
 
-	/** @persistent */
-	public $page = 1;
+    /** @var Paginator */
+    private $paginator;
 
+    /** @persistent */
+    public $page = 1;
 
+    /**
+     * @return Nette\Paginator
+     */
+    public function getPaginator() {
+        if (!$this->paginator) {
+            $this->paginator = new Paginator;
+        }
+        return $this->paginator;
+    }
 
-	/**
-	 * @return Nette\Paginator
-	 */
-	public function getPaginator()
-	{
-		if (!$this->paginator) {
-			$this->paginator = new Paginator;
-		}
-		return $this->paginator;
-	}
+    /**
+     * Renders paginator.
+     * @return void
+     */
+    public function render() {
+        $paginator = $this->getPaginator();
+        $page = $paginator->page;
+        if ($paginator->pageCount < 2) {
+            $steps = array($page);
+        } else {
+            $arr = range(max($paginator->firstPage, $page - 3), min($paginator->lastPage, $page + 3));
+            $count = 4;
+            $quotient = ($paginator->pageCount - 1) / $count;
+            for ($i = 0; $i <= $count; $i++) {
+                $arr[] = round($quotient * $i) + $paginator->firstPage;
+            }
+            sort($arr);
+            $steps = array_values(array_unique($arr));
+        }
 
+        $this->template->steps = $steps;
+        $this->template->paginator = $paginator;
+        $this->template->setFile(dirname(__FILE__) . '/template.latte');
+        $this->template->render();
+    }
 
-
-	/**
-	 * Renders paginator.
-	 * @return void
-	 */
-	public function render()
-	{
-		$paginator = $this->getPaginator();
-		$page = $paginator->page;
-		if ($paginator->pageCount < 2) {
-			$steps = array($page);
-
-		} else {
-			$arr = range(max($paginator->firstPage, $page - 3), min($paginator->lastPage, $page + 3));
-			$count = 4;
-			$quotient = ($paginator->pageCount - 1) / $count;
-			for ($i = 0; $i <= $count; $i++) {
-				$arr[] = round($quotient * $i) + $paginator->firstPage;
-			}
-			sort($arr);
-			$steps = array_values(array_unique($arr));
-		}
-
-		$this->template->steps = $steps;
-		$this->template->paginator = $paginator;
-		$this->template->setFile(dirname(__FILE__) . '/template.phtml');
-		$this->template->render();
-	}
-
-
-
-	/**
-	 * Loads state informations.
-	 * @param  array
-	 * @return void
-	 */
-	public function loadState(array $params)
-	{
-		parent::loadState($params);
-		$this->getPaginator()->page = $this->page;
-	}
+    /**
+     * Loads state informations.
+     * @param  array
+     * @return void
+     */
+    public function loadState(array $params) {
+        parent::loadState($params);
+        $this->getPaginator()->page = $this->page;
+    }
 
 }
